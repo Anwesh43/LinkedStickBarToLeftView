679b0252b39481cf195c621a453816f274692510
Anwesh43 <anweshthecool0@gmail.com>
2021-05-20 02:48:18 +0530

    adding constants and libraries

--- stickbartoleftview/src/main/java/com/example/stickbartoleftview/StickBarToLeftView.kt
+++ stickbartoleftview/src/main/java/com/example/stickbartoleftview/StickBarToLeftView.kt
@@ -1,2 +1,28 @@
 package com.example.stickbartoleftview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#4CAF50",
+    "#F57F17",
+    "#00C853",
+    "#311B92"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val deg : Float = 180f
+val strokeFactor : Float = 90f
+val lineSizeFactor : Float = 5.5f
+val sizeFactor : Float = 9.8f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
