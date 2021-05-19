4c63036a4fc73737759f32864f3061d763ff11ff
Anwesh43 <anweshthecool0@gmail.com>
2021-05-20 02:56:54 +0530

    adding Canvas extension methods

--- stickbartoleftview/src/main/java/com/example/stickbartoleftview/StickBarToLeftView.kt
+++ stickbartoleftview/src/main/java/com/example/stickbartoleftview/StickBarToLeftView.kt
@@ -31,3 +31,35 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawStickBarToLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val lSize : Float = Math.min(w, h) / lineSizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        save()
+        translate(-w * 0.5f * sf3, 0f)
+        rotate(deg * sf3)
+        drawRect(RectF(-size * sf1, -size / 2, 0f, size /2), paint)
+        drawLine(-size - lSize * sf2, 0f, -size, 0f, paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSBTLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawStickBarToLeft(scale, w, h, paint)
+}
