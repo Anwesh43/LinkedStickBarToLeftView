359b04cc70876f4d2ea22333f99fa140f97e19e4
Anwesh43 <anweshthecool0@gmail.com>
2021-05-20 02:59:06 +0530

    adding StickBarToLeftView class

--- stickbartoleftview/src/main/java/com/example/stickbartoleftview/StickBarToLeftView.kt
+++ stickbartoleftview/src/main/java/com/example/stickbartoleftview/StickBarToLeftView.kt
@@ -63,3 +63,20 @@ fun Canvas.drawSBTLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawStickBarToLeft(scale, w, h, paint)
 }
+
+
+class StickBarToLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
