464c96b61593e3d1c78c46df75a2c6ba9d85e24d
Anwesh43 <anweshthecool0@gmail.com>
2021-05-20 02:50:00 +0530

    adding Int anf Float extension methods

--- stickbartoleftview/src/main/java/com/example/stickbartoleftview/StickBarToLeftView.kt
+++ stickbartoleftview/src/main/java/com/example/stickbartoleftview/StickBarToLeftView.kt
@@ -26,3 +26,8 @@ val lineSizeFactor : Float = 5.5f
 val sizeFactor : Float = 9.8f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
