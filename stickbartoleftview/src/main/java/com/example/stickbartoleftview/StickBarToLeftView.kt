cd69d1bfecac12588456fae9a98cd4d1a51217e7
Anwesh43 <anweshthecool0@gmail.com>
2021-05-20 03:07:55 +0530

    adding SBTNode class

--- stickbartoleftview/src/main/java/com/example/stickbartoleftview/StickBarToLeftView.kt
+++ stickbartoleftview/src/main/java/com/example/stickbartoleftview/StickBarToLeftView.kt
@@ -127,4 +127,45 @@ class StickBarToLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBTLNode(var i : Int, val state : State = State()) {
+
+        private var next : SBTLNode? = null
+        private var prev : SBTLNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBTLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBTLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBTLNode {
+            var curr : SBTLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
